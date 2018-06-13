cc4490e48f50cc8950cae2869b86efe56eabf0e7
Protosac <protosacq@gmail.com>
2018-06-13 14:14:56 -0700

    Init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <!--Add nav component-->
+    <!--Add header component-->
+    <img src="./assets/logo.png">
+    <!-- Add so that route components are rendered in <App /> -->
+    <router-view/>
+
+    <!--Add footer component-->
+  </div>
+</template>
+
+<script>
+export default {
+  // This is the name the element will be called in the browser.
+  // It defaults to the name of the module, App(.vue)
+  name: 'App',
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/PageContainer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageContainer',
+  data() {
+    return {
+      message: 'Page container loaded.',
+    };
+  },
+};
+</script>
