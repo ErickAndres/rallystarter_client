122c7c2033410bb73b9dd7f5f76bbc6b88ab867b
Erick Andres <e.andres4@yahoo.com>
2018-08-11 00:44:36 +0000

    Background color & color text correct

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -8,9 +8,14 @@
     </div>
     <div class="wrapper">
       <div class="card" v-for='(rally, index) in rallies.data'>
-        <div class="rally_text">{{rally.total_actions}} Actions</div>
+        <div v-bind:class="classBackground(rally.categories[0])">
+          <div class="topcard">{{rally.categories[0]}}</div>
+          <div class="topcard">{{rally.total_actions}} Actions</div>
+        </div>
+        <!--<div class="rally_text">{{rally.total_actions}} Actions</div>-->
         <img class="img-with-text" v-bind:src="rally.image_url"/>
-        <p class="rally_text">{{rally.title}}</p>
+        <p v-bind:class="classText(rally.categories[0])">{{rally.title}}</p>
+        <!--<p class="rally_text">{{rally.title}}</p>-->
       </div>
     </div>
     <div class="align-right">
@@ -42,7 +47,7 @@ export default {
             summary: 'For decades the NRA has misinterpreted the 2nd Amendment in order to sell more guns therefore playing a major role as enablers in our mass shooting epidemic. The disrespect they\'ve shown to victims, survivors, and families effected by school shootings needs to be the last straw! Until the NRA is able to respect human rights and life more than they respect unregulated gun rights we\'re calling for a boycott of every company they do business with.   ',
             slug: 'boycott-the-nra',
             status: 'paid',
-            categories: '[\'Social Justice\',\'Public Safety\',\'Human Rights\',\'Politics\']',
+            categories: ['Social Justice','Public Safety','Human Rights','Politics'],
             featured: null,
             deleted: null,
             published: 1,
@@ -59,7 +64,7 @@ export default {
             summary: 'There is new evidence coming out daily that shows collusion between the Trump campaign and Russia as well as ongoing attempts by Team Trump to obstruct justice and discredit the FBI in order to undermine the investigation. \n\nTrump and his supporters are now angling to fire Special Council Robert Mueller and Deputy Attorney General Rod Rosenstein. Americans need to learn the truth! Call on Congress to pass legislation that will protect Mueller and Rosenstein so that the Special Council can do their job and continue these important investigations.',
             slug: 'protect-the-investigation-into-russia-s-us-election-interference',
             status: 'free',
-            categories: '[\'Politics\']',
+            categories: ['Politics'],
             featured: 1,
             deleted: null,
             published: 1,
@@ -76,7 +81,7 @@ export default {
             summary: 'Since pledging to recuse himself from the Trump-Russia investigation in April, Congressman Devin Nunes has continued to abuse his power to cover up for the Trump administration. With the GOP’s support, Nunes is attempting to undermine Mueller’s investigation by publicizing a memo that attacks the Trump-Russia investigation. Call on House Speaker Paul Ryan to use his power to remove Nunes as chair of the House Intelligence Committee. ',
             slug: 'remove-nunes-from-the-house-intelligence-committee',
             status: 'paid',
-            categories: '[\'Politics\']',
+            categories: ['Politics'],
             featured: null,
             deleted: null,
             published: 1,
@@ -93,7 +98,7 @@ export default {
             summary: 'The GOP tax plan, a bill designed to give the wealthy a large tax cut, passed along party lines without a single Democrat in Congress voting in favor of it. With permanent tax cuts for large corporations and expected budget cuts to Social Security and Medicaid, the GOP has clearly chosen to line their own pockets at the expense of the struggling American middle-class. Tell the GOP you\'ll remember this vote when they\'re up for re-election!',
             slug: 'replace-the-gop-repeal-the-tax-bill',
             status: 'free',
-            categories: '[\'Economy\',\'Health\',\'Politics\']',
+            categories: ['Economy','Health','Politics'],
             featured: 0,
             deleted: null,
             published: 1,
@@ -110,7 +115,7 @@ export default {
             summary: 'All true Americans need to stand up to Trump and demand that he stops attacking the 1st Amendment by discrediting the press and labeling negative media coverage as \'fake news\'. ',
             slug: 'protect-the-1st-amendment',
             status: 'free',
-            categories: '[\'Social Justice\',\'Media & Entertainment\',\'Politics\']',
+            categories: ['Social Justice','Media & Entertainment','Politics'],
             featured: 0,
             deleted: null,
             published: 1,
@@ -127,7 +132,7 @@ export default {
             summary: 'Twitter has a clearly defined policy related to violent threats and the glorification of violence. It states \'You may not make specific threats of violence or wish for the serious physical harm, death, or disease of an individual or group of people.\' Donald Trump has violated this policy on multiple occasions. Twitter needs to treat him the same as all of it\'s users and hold him accountable. ',
             slug: 'call-on-twitter-to-enforce-their-policies-and-ban-trump',
             status: 'free',
-            categories: '[\'Social Justice\',\'Public Safety\',\'Media & Entertainment\',\'Politics\']',
+            categories: ['Social Justice','Public Safety','Media & Entertainment','Politics'],
             featured: null,
             deleted: null,
             published: 1,
@@ -138,7 +143,35 @@ export default {
         ],
       },
     };
-  }, 
+  },
+  methods: {
+    classBackground: function(arg){
+      var theClass = '';
+      if (arg === 'Arts & Culture' || arg === 'Public Safety' || arg === 'LGBTQ Rights' || arg === "Women's Rights") {
+        theClass = 'purplevue';
+      } else if (arg === "Children's Issues" || arg === "Economy" || arg === "Veteran's Issues") {
+        theClass = 'jadevue';
+      } else if (arg === 'Social Justice' || arg === 'Education' || arg === 'Environment' || arg === 'Media & Entertainment') {
+        theClass = 'tealvue';
+      } else {
+        theClass = 'orangevue';
+      }
+      return theClass;
+    },
+    classText: function(arg){
+      var theClass = '';
+      if (arg === 'Arts & Culture' || arg === 'Public Safety' || arg === 'LGBTQ Rights' || arg === "Women's Rights") {
+        theClass = 'purplevuep';
+      } else if (arg === "Children's Issues" || arg === "Economy" || arg === "Veteran's Issues") {
+        theClass = 'jadevuep';
+      } else if (arg === 'Social Justice' || arg === 'Education' || arg === 'Environment' || arg === 'Media & Entertainment') {
+        theClass = 'tealvuep';
+      } else {
+        theClass = 'orangevuep';
+      }
+      return theClass;
+    }
+  },
 };
 </script>
 
@@ -241,5 +274,34 @@ h5{
     margin: 0 auto;
     display: table-cell;
     vertical-align: middle;
+}
+.topcard {
+  text-transform: uppercase;
+  color: #fff;
+}
+.purplevue { 
+  background-color: #800080;
+}
+.tealvue {
+  background-color: #2F6D87;
+}
+.jadevue {
+  background-color: #5bcbbc;
+}
+.orangevue {
+  background-color: #f95607;
+}
+
+.purplevuep { 
+  color: #800080;
+}
+.tealvuep {
+  color: #2F6D87;
+}
+.jadevuep {
+  color: #5bcbbc;
+}
+.orangevuep {
+  color: #f95607;
 }
 </style>
